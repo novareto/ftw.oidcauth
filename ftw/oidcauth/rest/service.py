89c56ce93e9e0863b0cae3641640cc476fe7d712
cklinger <ck@novareto.de>
2023-03-29 10:27:46 +0200

    Changes REST Keycloak

--- ftw/oidcauth/rest/service.py
+++ ftw/oidcauth/rest/service.py
@@ -1,8 +1,12 @@
 import json
+import base64
+from binascii import b2a_base64, a2b_base64
+from AccessControl import AuthEncoding
 from plone import api
 from plone.rest import Service
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
+from Products.CMFCore.utils import getToolByName
 
 
 class User(dict):
@@ -21,7 +25,9 @@ class KeyCloakService(Service):
         return self
 
     def render(self):
-        print self.params
+        print(self.params)
+        AuthEncoding.pw_encrypt('danny')
+        api.user.create(email="ck@nn.de", username="cklinger", password="passwort")
         if len(self.params) == 0:
             ret = []
             for user in api.user.get_users():
@@ -45,4 +51,43 @@ class KeyCloakService(Service):
                         fristName='fN', #user.getProperty('firstName'),
                         lastName='lN', #user.getProperty('lastName')
                     )
+        print(ret)
+        return json.dumps(ret)
+
+
+class PWResult(dict):
+    pass
+
+
+@implementer(IPublishTraverse)
+class KeyCloakCredentials(Service):
+
+    def __init__(self, context, request):
+        super(KeyCloakCredentials, self).__init__(context, request)
+        self.params = []
+
+    def publishTraverse(self, request, name):
+        self.params.append(name)
+        return self
+
+    def render(self):
+        print(self.params)
+        uf = getToolByName(self, 'acl_users')
+        username = self.params[0]
+        user = api.user.get(username=username)
+        pw = uf.source_users._user_passwords.get(username, '')
+        pw_b64 = a2b_base64(pw[6:])
+        salt = pw_b64[20:]  #.decode('utf-8')
+        salt = base64.b64encode(salt).decode('utf-8')
+        value = pw_b64[:20]
+        value = base64.b64encode(value).decode('utf-8')
+
+        ret = PWResult(
+            value=value,
+            salt=salt,
+            iterations=0,
+            algorithm= "SSHA",
+            type= "password"
+        )
+        print(ret)
         return json.dumps(ret)
